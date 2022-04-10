7e97a7ce00a06b1fa6851aecbbac5a292415af30
Debashis Biswas <debashis.biswas@asu.edu>
2022-04-10 15:35:35 -0700

    Refactored audio handling into AudioManager class

---
+++ src/audio.ts
@@ -0,0 +1,101 @@
+import { AudioListener, Audio, AudioAnalyser } from 'three';
+
+export default class AudioManager {
+    listener: AudioListener;
+    audio: Audio;
+    three_analyser: AudioAnalyser;
+    readonly noteStrings = [
+        'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'
+    ];
+
+    constructor() {
+        this.listener = new AudioListener();
+        this.audio = new Audio(this.listener);
+        this.three_analyser = new AudioAnalyser(this.audio, 2048);
+
+        navigator.mediaDevices.getUserMedia({
+            audio: {
+                autoGainControl: false,
+                echoCancellation: false,
+                latency: 0,
+                noiseSuppression: false,
+            } as MediaTrackConstraints,
+        })
+            .then(this._handleGetMicSuccess)
+            .catch(console.error);
+    }
+
+    private _handleGetMicSuccess = (stream: MediaStream) => {
+        const source = this.listener.context.createMediaStreamSource(stream);
+        this.audio.setMediaStreamSource(source.mediaStream);
+        source.connect(this.three_analyser.analyser);
+        console.log('Audio successfully attached!');
+    };
+
+    get analyserNode(): AnalyserNode {
+        return this.three_analyser.analyser;
+    }
+
+    findFundamentalFreq = (buf: Float32Array): number =>
+    {
+        // Implements the ACF2+ algorithm
+        let SIZE = buf.length;
+        let rms = 0;
+
+        for (let i = 0;i < SIZE;i++) {
+            const val = buf[i];
+            rms += val * val;
+        }
+        rms = Math.sqrt(rms / SIZE);
+        if (rms < 0.01) // not enough signal
+            return -1;
+
+        let r1 = 0, r2 = SIZE - 1;
+        const thres = 0.2;
+        for (let i = 0; i < SIZE / 2; i++)
+            if (Math.abs(buf[i]) < thres) { r1 = i; break; }
+        for (let i = 1; i < SIZE / 2; i++)
+            if (Math.abs(buf[SIZE - i]) < thres) { r2 = SIZE - i; break; }
+
+        buf = buf.slice(r1, r2);
+        SIZE = buf.length;
+
+        const c = Float32Array.from(new Array(SIZE).fill(0));
+        for (let i = 0; i < SIZE; i++)
+            for (let j = 0; j < SIZE - i; j++)
+                c[i] = c[i] + buf[j] * buf[j + i];
+
+        let d = 0; while (c[d] > c[d + 1]) d++;
+        let maxval = -1, maxpos = -1;
+
+        for (let i = d; i < SIZE; i++) {
+            if (c[i] > maxval) {
+                maxval = c[i];
+                maxpos = i;
+            }
+        }
+        let T0 = maxpos;
+
+        const x1 = c[T0 - 1], x2 = c[T0], x3 = c[T0 + 1];
+        const a = (x1 + x3 - 2 * x2) / 2;
+        const b = (x3 - x1) / 2;
+        if (a) T0 = T0 - b / (2 * a);
+
+        return this.audio.context.sampleRate / T0;
+    };
+
+    noteFromPitch = (frequency: number) => {
+        const noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
+        return Math.round(noteNum) + 69;
+    };
+
+    frequencyFromNoteNumber = (note: number) => {
+        return 440 * Math.pow(2, (note - 69) / 12);
+    };
+
+    centsOffFromPitch = (frequency: number, note: number) => {
+        return Math.floor(
+            1200 * Math.log(frequency / this.frequencyFromNoteNumber(note)) / Math.log(2)
+        );
+    };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import * as TWEEN from '@tweenjs/tween.js';
 import * as THREE from 'three';
+import AudioManager from './audio';
 import dimensions from './dimensions';
 import Player from './player';
 import view from './view';
+
+const audioManager = new AudioManager();
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const player = new Player();
+const objects: Array<THREE.Mesh> = [];
+const ui = document.getElementById('center-ui');
 
 const camera = new THREE.OrthographicCamera(
     -view.width / 2,
@@ -11,87 +19,7 @@ const camera = new THREE.OrthographicCamera(
     -view.height / 2,
 );
 
-const listener = new THREE.AudioListener();
-const audio = new THREE.Audio(listener);
-const analyser = new THREE.AudioAnalyser(audio, 2048);
-camera.add(listener);
-
-function handleSuccess(stream: MediaStream) {
-    const context = listener.context;
-    const source = context.createMediaStreamSource(stream);
-    audio.setMediaStreamSource(source.mediaStream);
-    source.connect(analyser.analyser);
-    console.log('Audio successfully attached!');
-}
-
-navigator.mediaDevices.getUserMedia({
-    audio: {
-        autoGainControl: false,
-        echoCancellation: false,
-        latency: 0,
-        noiseSuppression: false,
-    } as MediaTrackConstraints,
-})
-    .then(handleSuccess)
-    .catch(console.error);
-
-const findFundamentalFreq = (buf: Float32Array, sampleRate: number): number =>
-{
-    // Implements the ACF2+ algorithm
-    let SIZE = buf.length;
-    let rms = 0;
-
-    for (let i = 0;i < SIZE;i++) {
-        const val = buf[i];
-        rms += val * val;
-    }
-    rms = Math.sqrt(rms / SIZE);
-    if (rms < 0.01) // not enough signal
-        return -1;
-
-    let r1 = 0, r2 = SIZE - 1;
-    const thres = 0.2;
-    for (let i = 0; i < SIZE / 2; i++)
-        if (Math.abs(buf[i]) < thres) { r1 = i; break; }
-    for (let i = 1; i < SIZE / 2; i++)
-        if (Math.abs(buf[SIZE - i]) < thres) { r2 = SIZE - i; break; }
-
-    buf = buf.slice(r1, r2);
-    SIZE = buf.length;
-
-    const c = Float32Array.from(new Array(SIZE).fill(0));
-    for (let i = 0; i < SIZE; i++)
-        for (let j = 0; j < SIZE - i; j++)
-            c[i] = c[i] + buf[j] * buf[j + i];
-
-    let d = 0; while (c[d] > c[d + 1]) d++;
-    let maxval = -1, maxpos = -1;
-
-    for (let i = d; i < SIZE; i++) {
-        if (c[i] > maxval) {
-            maxval = c[i];
-            maxpos = i;
-        }
-    }
-    let T0 = maxpos;
-
-    const x1 = c[T0 - 1], x2 = c[T0], x3 = c[T0 + 1];
-    const a = (x1 + x3 - 2 * x2) / 2;
-    const b = (x3 - x1) / 2;
-    if (a) T0 = T0 - b / (2 * a);
-
-    return sampleRate / T0;
-};
-
-const getScoreElement = (): HTMLElement | null => {
-    return document.getElementById('center-ui');
-};
-
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-const player = new Player();
-const objects: Array<THREE.Mesh> = [];
-const ui = getScoreElement();
+camera.add(audioManager.listener);
 
 const checkCollisions = () => {
     const circle = new THREE.Sphere();
@@ -131,40 +59,22 @@ const updateUI = (score: string) => {
     ui.innerHTML = score;
 };
 
-const noteStrings = [
-    'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'
-];
-
-function noteFromPitch(frequency: number) {
-    const noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
-    return Math.round(noteNum) + 69;
-}
-
-function frequencyFromNoteNumber(note: number) {
-    return 440 * Math.pow(2, (note - 69) / 12);
-}
-
-function centsOffFromPitch(frequency: number, note: number) {
-    return Math.floor(
-        1200 * Math.log(frequency / frequencyFromNoteNumber(note)) / Math.log(2)
-    );
-}
-
 const animation = (time: number) => {
     renderer.render(scene, camera);
     TWEEN.update(time);
     player.applyMovement();
     checkCollisions();
 
-    const buffer = new Float32Array(analyser.analyser.fftSize);
-    analyser.analyser.getFloatTimeDomainData(buffer);
-    const fundamentalFreq = findFundamentalFreq(
-        buffer, audio.context.sampleRate
-    );
-    const noteNumber: number = noteFromPitch(fundamentalFreq);
-    const note: string = noteStrings[noteNumber % 12];
+    const analyzerNode = audioManager.analyserNode;
+    const buffer = new Float32Array(analyzerNode.fftSize);
+    analyzerNode.getFloatTimeDomainData(buffer);
+    const fundamentalFreq = audioManager.findFundamentalFreq(buffer);
+    const noteNumber: number = audioManager.noteFromPitch(fundamentalFreq);
+    const note: string = audioManager.noteStrings[noteNumber % 12];
     if (note) {
-        updateUI(`${note}, ${centsOffFromPitch(fundamentalFreq, noteNumber)}`);
+        updateUI(`${note}, ${
+            audioManager.centsOffFromPitch(fundamentalFreq, noteNumber)
+        }`);
     }
     else {
         updateUI('');
